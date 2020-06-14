4642fe7f3cddd4e2def4fb45aa0a7581954a0920
mathieu Kapfer <mathieu.kapfer@non.se.com>
2020-06-14 18:11:15 +0200

    [projetile] add projectile git cmd

--- init.el
+++ init.el
@@ -9,11 +9,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\.[^.]")
+ '(c-basic-offset 2)
+ '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.[^.]")
+ '(fill-column 80)
+ '(indent-tabs-mode nil)
  '(package-archives (quote (("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(package-selected-packages
    (quote
-    (auto-complete-clang-async auto-complete-clang auto-complete flycheck-cstyle flycheck projectile ggtags yasnippet yasnippet-snippets use-package magit))))
+    (dash git-commit popup transient with-editor irony rtags cmake-ide ac-clang clang-format elpy auto-complete-clang-async auto-complete-clang auto-complete flycheck-cstyle flycheck projectile ggtags yasnippet yasnippet-snippets use-package magit)))
+ '(projectile-git-command "git ls-files -zc --exclude-standard  --recurse-submodules")
+ '(tab-width 2))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
