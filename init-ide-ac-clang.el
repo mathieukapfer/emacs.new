60bc05efe26f664263a6fd9acf6ef2bd3f24e261
Kapfer Family <kapfer@debian-desktop>
2020-06-14 17:54:01 +0200

    [ac] setup cmake-ide + clang async

--- init-ide-ac-clang.el
+++ init-ide-ac-clang.el
@@ -1,11 +1,29 @@
+;; http://github.com/atilaneves/cmake-ide
+(cmake-ide-setup)
 
+(setq cmake-ide-flags-c++ ;;ac-clang-flags
+      (mapcar (lambda (item)(concat "-I" item))
+              ;; provide by: echo "" | g++ -v -x c++ -E -
+              ;; (from https://github.com/brianjcj/auto-complete-clang #Troubleshooting)
+               (split-string
+                "
+ /usr/include/c++/8
+ /usr/include/x86_64-linux-gnu/c++/8
+ /usr/include/c++/8/backward
+ /usr/lib/gcc/x86_64-linux-gnu/8/include
+ /usr/local/include
+ /usr/lib/gcc/x86_64-linux-gnu/8/include-fixed
+ /usr/include/x86_64-linux-gnu
+ /usr/include
+"
+                )))
 
 ;; https://github.com/Golevka/emacs-clang-complete-async
 
 (require 'auto-complete-clang-async)
 
 (defun ac-cc-mode-setup ()
-  (setq ac-clang-complete-executable "~/.emacs.d/emacs-clang-complete-async/clang-complete")
+  (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
   ;;(setq ac-sources '(ac-source-clang-async))
   ;;(setq ac-sources (append '(ac-source-clang-async) ac-sources))
   (setq ac-sources '(ac-source-clang-async ac-source-yasnippet))
@@ -16,6 +34,7 @@
   (add-hook 'c++-mode-common-hook 'ac-cc-mode-setup)
   (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
   (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (add-hook 'after-save-hook 'ac-clang-update-cmdlineargs) ;; make cflags change to take effect
   (global-auto-complete-mode t))
 
 (my-ac-config)
--- init.el
+++ init.el
@@ -3,11 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
  '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\.[^.]")
+ '(fill-column 80)
+ '(indent-tabs-mode nil)
+ '(jdee-jdk-registry
+   (quote
+    (("10.0" . "/usr/lib/jvm/java-1.11.0-openjdk-amd64/"))))
+ '(jdee-server-dir "/home/kapfer/Code-git/jdee-server/target")
  '(package-archives (quote (("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(package-selected-packages
    (quote
-    (auto-complete-clang-async auto-complete-clang auto-complete flycheck-cstyle flycheck projectile ggtags yasnippet yasnippet-snippets use-package magit))))
+    (cmake-ide cmake-mode jdee auto-complete auto-complete-clang-async   flycheck-cstyle flycheck projectile ggtags yasnippet yasnippet-snippets use-package magit))) ;; auto-complete-clang
+ '(tab-width 2))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
