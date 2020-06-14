c9daca0870f5425eca3263c14877adcec3e1405e
mathieu Kapfer <mathieu.kapfer@non.se.com>
2020-06-14 18:08:32 +0200

    [clang] installation

--- run_install.sh
+++ run_install.sh
@@ -4,7 +4,9 @@ sudo apt-get install elpa-flycheck flycheck-doc
 
 # clang completion setup
 #  - install packages
-sudo apt-get install clang libclang-7-dev llvm
+sudo apt-get install clang llvm llvm-dev
 #  - make clang-complete
+git submodule init
+git submodule update
 cd emacs-clang-complete-async && make
 
